dd636feeed6bac8cf9d4d65b8d2a5847ee0839a6
Natalia Castro <nat.alvz@gmail.com>
2018-05-16 22:47:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,5 @@ def reverse_each_word(sentence)
   sentence = string.split(" ")
   new_sentence = []
  reverse_each_word.each do|word|
- end_loop = word.length
-		new_string = ''
-		while end_loop > 0
-			end_loop -= 1
-			new_string += word[end_loop] 
-		end
-		new_sentence << new_string 
-	end
-	new_sentence.join(' ')
+
 end
